cad7267a9d026fef4a8fe1ad319687341238947d
Davies, Christopher <christopher.davies2@sky.uk>
2019-06-28 16:38:43 +0100

    Added logging to help diagnose issue.

--- src/test/java/com/automatedtesting/steps/LiveFlightSearchCreateSessionSteps.java
+++ src/test/java/com/automatedtesting/steps/LiveFlightSearchCreateSessionSteps.java
@@ -7,13 +7,11 @@ import io.restassured.config.*;
 import io.restassured.http.*;
 import io.restassured.response.*;
 import io.restassured.specification.*;
-import org.apache.log4j.Logger;
+import org.apache.log4j.*;
 import org.springframework.beans.factory.annotation.*;
 
 import java.util.*;
-import java.util.logging.*;
 
-import static io.restassured.config.EncoderConfig.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class LiveFlightSearchCreateSessionSteps implements En {
@@ -21,6 +19,7 @@ public class LiveFlightSearchCreateSessionSteps implements En {
     private static final String X_RAPID_API_HOST = "X-RapidAPI-Host";
     private static final String X_RAPID_API_KEY = "X-RapidAPI-Key";
     private static final String SERVICES_PRICING_V_1_0 = "/apiservices/pricing/v1.0";
+    private static final Logger LOG = Logger.getLogger(LiveFlightSearchCreateSessionSteps.class);
 
     private RequestSpecification request = RestAssured.given();
     private Response response;
@@ -42,7 +41,6 @@ public class LiveFlightSearchCreateSessionSteps implements En {
 
         When("^I call POST \\/services\\/pricing\\/v1.0 with the following parameters:$", (DataTable dataTable) -> {
             request.basePath(SERVICES_PRICING_V_1_0);
-
             request.header(X_RAPID_API_HOST, skyscannerAPIHost);
             request.header(X_RAPID_API_KEY, skyscannerAPIKey);
             request.config(RestAssured.config()
@@ -53,12 +51,14 @@ public class LiveFlightSearchCreateSessionSteps implements En {
             Map<String, String> params = dataTable.asMap(String.class, String.class);
             request.formParams(params);
 
-
+            LOG.info(request.log().method().toString());
+            LOG.info(request.log().uri().toString());
+            LOG.info(request.log().params().toString());
 
             response = request.post();
         });
 
-        Then("^the response will return a HTTP (\\d\\d\\d) status$", (Integer statusCode) -> {
+        Then("^the response will return a HTTP (\\d{3}) status$", (Integer statusCode) -> {
             System.out.println(response.body().asString());
             assertThat(response.statusCode()).isEqualTo(statusCode);
         });
